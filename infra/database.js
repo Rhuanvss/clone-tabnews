6b7a39a9240fcf081601cccd1a75ea318d8194ae
Rhuanvss <rhuanvictor1323@gmail.com>
2026-01-12 01:00:13 +0000

    adicionado conexao ssl no database

--- infra/database.js
+++ infra/database.js
@@ -1,18 +1,9 @@
 import { Client } from "pg";
+
 async function query(queryObject) {
   const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-  });
-  console.log("Connecting to database...", {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
+    connectionString: process.env.DATABASE_URL,
+    ssl: process.env.NODE_ENV === "production" ? true : false, // SSL ativo apenas em produção (Vercel)
   });
 
   try {
@@ -20,7 +11,7 @@ async function query(queryObject) {
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
-    console.error("Database query error:", error);
+    console.error(error);
     throw error;
   } finally {
     await client.end();
